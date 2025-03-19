10d2e8ef14ba2da5fe450cd00050a04581b8998b
kamalpreet singh <kamaljaner445@gmail.com>
2025-03-19 17:14:09 -0600

    update

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ const app = express();
 const PORT = process.env.PORT;
 
 //router handler that will define url/url that does something
-app.get('/home',(req,res)=>{
-  res.send('hello world!');
+app.post('/user/:id', (req, res) => {
+  const user_id = req.params.id;
+  res.send(`User ID is: ${user_id}`);
+});
+
+//router handler
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+  res.send(`This is the search term entered: ${query}`);
 });
 
 //start our server 
 app.listen(PORT, () => {
-  console.log('server started on port: ${PORT}');
+  console.log(`server started on port: ${PORT}`);
 });
+
